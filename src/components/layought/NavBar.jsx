931aa57a376576d89962e4b2e7236bc29ea78286
Anand Kadale <anandkadale1@gmail.com>
2023-03-30 12:23:08 +0000

    Update NavBar.jsx

--- src/components/layought/NavBar.jsx
+++ src/components/layought/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import PropTypes from "proptypes";
+import PropTypes from "prop-types";
 
 function NavBar({ title }) {
   return (
