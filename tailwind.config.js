8202d5eca50a0b972d9664c0a5722b371fd290e0
Anandk008 <Anandkadale1@gmail.com>
2023-03-30 22:52:33 +0530

    Basic Done2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
+  content: [
+  "./src/**/*.{js,jsx,ts,tsx}",
+ ],
+ theme: {
+  extend: {},
+ },
+ plugins:[require('daisyui')],
 }
-
