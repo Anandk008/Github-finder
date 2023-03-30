8f248e6c0178ffe4ee67ed191d1d877552445bee
Anandk008 <Anandkadale1@gmail.com>
2023-03-30 16:39:20 +0530

    First

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from "proptypes";
+
+function NavBar() {
+  return <div> NavBar</div>;
+}
+
+
+export default NavBar;
